5f5db2ea6ded77381e084f33e4b2dd6b2c171b24
Robinson C <51316342+x-N0@users.noreply.github.com>
2021-02-24 23:41:35 -0300

    feat(projects): Consume content api data.

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,21 @@
     <indexHeader></indexHeader>
     <about-me></about-me>
     <TrustedBy></TrustedBy>
-    <show-case></show-case>
+    <show-case :projectsArray="projectsArray"></show-case>
     <TimelineMidStraight></TimelineMidStraight>
     <!--    <my-services></my-services>-->
     <!--    <contact-me></contact-me>-->
-    <!--    <project-viewer :advanced-mode="false"></project-viewer>-->
+    <project-viewer :advanced-mode="false"></project-viewer>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue } from 'nuxt-property-decorator'
 import indexHeader from '~/components/pages/index/indexHeader.vue'
 import AboutMe from '~/components/pages/index/AboutMe.vue'
 import ShowCase from '~/components/pages/index/ShowCase.vue'
-import ContactMe from '~/components/pages/index/contactMe.vue'
-import MyServices from '~/components/pages/index/myServices.vue'
+// import ContactMe from '~/components/pages/index/contactMe.vue'
+// import MyServices from '~/components/pages/index/myServices.vue'
 import ProjectViewer from '~/components/misc/ProjectViewer.vue'
 import TrustedBy from '~/components/pages/index/TrustedBy.vue'
 import TimelineMidStraight from '~/components/pages/index/TimelineMidStraight.vue'
@@ -25,17 +25,24 @@ export default Vue.extend({
   components: {
     TimelineMidStraight,
     TrustedBy,
-    MyServices,
-    ContactMe,
+    // MyServices,
+    // ContactMe,
     ShowCase,
     AboutMe,
     indexHeader,
     ProjectViewer,
   },
+  async asyncData({ $content, params }: any) {
+    const projectsData = await $content('projects', params.slug)
+      .sortBy('createdAt', 'asc')
+      .fetch()
+    return {
+      projectsArray: projectsData,
+    }}
 })
 </script>
 
-<style>
+<style lang="css">
 .container {
   margin: 0 auto;
   min-height: 100vh;
