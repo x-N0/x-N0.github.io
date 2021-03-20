68f7ad665e34a72b7517001fc901896ff98b54c5
Robinson C <51316342+x-N0@users.noreply.github.com>
2021-03-20 08:25:02 -0300

    fix(eslint): Adapt changes to chinitos one.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,24 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     project: './tsconfig.json',
-    ecmaFeatures: { "legacyDecorators": true }
+    ecmaFeatures: { legacyDecorators: true },
   },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
     'prettier/vue',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
   ],
-  plugins: [
-    'prettier',
-    '@typescript-eslint'
-  ],
-  // add your custom rules here
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
-  }
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
 }
