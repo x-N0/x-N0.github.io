669397eeada2beae88d2b467d733ae6db80eaf29
Robinson C <51316342+x-N0@users.noreply.github.com>
2021-03-20 07:19:49 -0300

    chore(eslint): Update eslint as the chinitos one.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: { "legacyDecorators": true }
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
+    '@nuxtjs',
     'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+    'prettier/@typescript-eslint'
   ],
-  plugins: ['prettier'],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
 }
