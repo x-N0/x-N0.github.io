3d60c514975744cbe7d74a63fa721fd7d6094cdb
Robinson C <51316342+x-N0@users.noreply.github.com>
2021-03-29 04:58:49 -0300

    feat(dynamic_viewer): Poc dynamic contentful viewer modal.

---
+++ pages/projects/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ projects }}</div>
+</template>
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator'
+
+export default Vue.extend({
+  async asyncData({ $content, params }: any) {
+    const projectsData = await $content('projects', params.slug)
+      .only(['title', 'description', 'img', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+    return {
+      projects: projectsData
+    }
+  }
+})
+</script>
