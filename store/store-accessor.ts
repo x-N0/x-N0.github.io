2458c465a592205625d5a588f5f689c4cecddb56
Robinson C <51316342+x-N0@users.noreply.github.com>
2021-03-29 04:55:50 -0300

    chore: Del badly storage files.

--- store/store-accessor.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from 'vuex'
-import { getModule } from 'vuex-module-decorators'
-import ProjectViewerStore from './projectViewerStore'
-
-export function getProjectViewer(store: Store<any>): ProjectViewerStore {
-  return getModule(ProjectViewerStore, store)
-}
